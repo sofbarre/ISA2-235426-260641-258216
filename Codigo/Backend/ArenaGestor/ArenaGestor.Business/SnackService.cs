ad86333628d3daecc5959c78bd9f29e33f9a7274
Facha0367 <bruno.facello15@gmail.com>
2023-05-10 22:00:43 -0300

    Creacion de funcionalidades de mantenimiento de snacks

--- Codigo/Backend/ArenaGestor/ArenaGestor.Business/SnackService.cs
+++ Codigo/Backend/ArenaGestor/ArenaGestor.Business/SnackService.cs
@@ -22,24 +22,54 @@ namespace ArenaGestor.Business
 
         public void InsertSnack(Snack snack)
         {
-            
+            if (snack.Name.Trim().Equals("") || snack.Price < 0)
+            {
+                throw new Exception("Precio o nombre de snack no validos");
+            }
+            else
+            {
+                this.snackManagement.InsertSnack(snack);
+            }
+
         }
 
         public Snack GetSnackById(int snackId)
         {
-            return null;
+            Snack snack = this.snackManagement.GetSnackById(snackId);
+            if (snack == null)
+            {
+                throw new Exception("Snack no eoncontrado");
+            }
+            else
+            {
+                return snack;
+            }
         }
         public IEnumerable<Snack> GetSnacks()
         {
-            return null;
+            return this.snackManagement.GetSnacks();
         }
         public void UpdateSnack(Snack sanckUpdated)
         {
-
+            if (sanckUpdated.Name.Trim().Equals("") || sanckUpdated.Price < 0)
+            {
+                throw new Exception("Precio o nombre de snack no validos");
+            }
+            else
+            {
+                this.snackManagement.UpdateSnack(sanckUpdated);
+            }
         }
         public void DeleteSnack(int snackId)
         {
-            
+            if (GetSnackById(snackId) == null)
+            {
+                throw new Exception("Snack inexistente");
+            }
+            else
+            {
+                this.snackManagement.DeleteSnack(snackId);
+            }
         }
     }
 }
--- Codigo/Backend/ArenaGestor/ArenaGestor.DataAccess/Managements/SnackManagement.cs
+++ Codigo/Backend/ArenaGestor/ArenaGestor.DataAccess/Managements/SnackManagement.cs
@@ -20,23 +20,32 @@ namespace ArenaGestor.DataAccess.Managements
         }
         public void InsertSnack(Snack snack)
         {
-            
+            context.Snacks.Add(snack);
+            context.SaveChanges();
+
         }
         public IEnumerable<Snack> GetSnacks()
         {
-            return null;
+            return context.Snacks.ToList().OrderBy(x => x.SnackId);
         }
         public Snack GetSnackById(int id)
         {
-            return null;
+            return context.Snacks.FirstOrDefault(snack => snack.SnackId == id);
         }
         public void UpdateSnack(Snack snackUpdated)
         {
-            
+            Snack snackDB = context.Snacks.FirstOrDefault(snack => snack.SnackId == snackUpdated.SnackId);
+            snackDB.Name = snackUpdated.Name;
+            snackDB.Description = snackUpdated.Description;
+            snackDB.Price = snackUpdated.Price;
+            context.Snacks.Update(snackDB);
+            context.SaveChanges();
         }
         public void DeleteSnack(int snackId)
         {
-            
+            Snack snackDB = context.Snacks.FirstOrDefault(snack => snack.SnackId == snackId);
+            context.Snacks.Remove(snackDB);
+            context.SaveChanges();
         }
     }
 }
